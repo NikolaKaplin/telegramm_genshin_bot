e79728a6ea66a3fbb70180d8a558d5fd5d89dd1d
NikolaKaplin <kaplinkola0@gmail.com>
2024-02-13 12:16:24 +0300

    removed false activations
    Co-authored-by: Ruslan Gulid <EcStud@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import TelegramBot from 'node-telegram-bot-api';
-import { findCharactersByRussianName, getCharacters } from './types/Character';
+import TelegramBot from "node-telegram-bot-api";
+import { findCharactersByRussianName, getCharacters } from "./types/Character";
 
-const token = '6687835699:AAH-BxxK8JmN6upta6VSmPNPC2xQjfpTbMA';
+const token = "6687835699:AAH-BxxK8JmN6upta6VSmPNPC2xQjfpTbMA";
 
 getCharacters();
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/list/, async (msg) => {
   const characters = await getCharacters();
 
-  bot.sendMessage(msg.chat.id, "Список персонажей:\n" + characters.map(c => c.name).join(", "));
-})
+  bot.sendMessage(
+    msg.chat.id,
+    "Список персонажей:\n" + characters.map((c) => c.name).join(", ")
+  );
+});
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
@@ -18,22 +21,16 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, "Привет, я готов тебе помочь");
 });
 
-bot.onText(/.*/, async (msg) => {
+bot.onText(/^((?!\/).)*$/, async (msg) => {
   const query = msg.text || "";
   const characters = await findCharactersByRussianName(query);
-  if (query != "/list") {
-    if (characters.length > 1) {
-      const response = bot.sendMessage(msg.chat.id, "Возможно, вы имели ввиду:", {
-        reply_markup: {
-          inline_keyboard: characters.map(c => (
-            [
-              {text: c.name, url: c.page}
-            ]
-          ))
-        }
-      });
-    } else {
-      bot.sendMessage(msg.chat.id, `Найден персонаж: ${characters[0].name}`);
-    }
+  if (characters.length > 1) {
+    const response = bot.sendMessage(msg.chat.id, "Возможно, вы имели ввиду:", {
+      reply_markup: {
+        inline_keyboard: characters.map((c) => [{ text: c.name, url: c.page }]),
+      },
+    });
+  } else {
+    bot.sendMessage(msg.chat.id, `Найден персонаж: ${characters[0].name}`);
   }
 });
